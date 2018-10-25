0e6594d12b2298c9816ef62d670d7f0e635d0818
JayBo7 <jvboinapalli@ucdavis.edu>
2018-10-25 15:47:33 -0700

    generate random prices, ratings, and number of reviews

--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,13 @@
 var location = require('./Location.js');
+var faker = require('faker');
 
-location.Listing.create({}, function(err, listing) {
-	if (err) throw err;
+for (var i = 0; i < 100; i++) {
+
+	location.Listing.create({
+		pricing: faker.commerce.price(),
+		average_review: (Math.random() * 5),
+		total_reviews: (Math.floor(Math.random() * 200))
+	}, function(err, listing) {
+		if (err) throw err;
+	}
 }
