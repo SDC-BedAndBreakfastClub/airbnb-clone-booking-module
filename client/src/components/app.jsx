b2f10afc464964efca6936188cab29da972736e5
JayBo7 <jvboinapalli@ucdavis.edu>
2018-10-30 11:30:37 -0700

    fix eslint issues and make code more readable

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
-import Guest from './guest.jsx';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			clicked: false
-		}
-	}
+    this.state = {
+      clicked: false,
+    };
+  }
 
-	handleClick() {
-		this.setState({
-			clicked: true
-		})
-	}
+  handleClick() {
+    this.setState({
+      clicked: true,
+    });
+  }
 
-	// conditional rendering
+  // conditional rendering
 
-	render() {
-		if (!this.state.clicked) {
-			return (
-				<div>
-					<input type="date" id="start" />
-					<input type="date" id="end" />
-					<Guest />
-				</div>
-			)
-		} else {
-			return (
-				<div><p>Nice Click</p></div>
-			)
-		}
-	}
+  render() {
+    const { clicked } = this.state;
+    if (!clicked) {
+      return (
+        <div>
+          <input type="button" onClick={this.handleClick.bind(this)} />
+          <input type="date" id="start" />
+          <input type="date" id="end" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input type="button" />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
--- client/src/components/guest.jsx
+++ client/src/components/guest.jsx
@@ -1,48 +1,55 @@
 import React from 'react';
 
 class Guest extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			drop: false
-		}
+    this.state = {
+      drop: false,
+    };
 
-		this.handleDrop = this.handleDrop.bind(this);
-		this.handleClose = this.handleClose.bind(this);
-	}
+    this.handleDrop = this.handleDropMenu.bind(this);
+    this.handleClose = this.handleCloseMenu.bind(this);
+  }
 
-	handleDrop(event) {
-		event.preventDefault();
+  handleDropMenu(event) {
+    event.preventDefault();
 
-		this.setState({
-			drop: true
-		}, () => document.addEventListener('click', this.handleClose))
-	}
+    this.setState({
+      drop: true,
+    }, () => document.addEventListener('click', this.handleClose));
+  }
 
-	handleClose(event) {
+  handleCloseMenu(event) {
+    if (!this.menu.contains(event.target)) {
+      this.setState({
+        drop: false,
+      }, () => document.removeEventListener('click', this.handleClose));
+    }
+  }
 
-		if (!this.menu.contains(event.target)) {
-			this.setState({
-				drop: false
-			}, () => document.removeEventListener('click', this.handleClose))
-		}
-	}
+  render() {
+    const { drop } = this.state;
 
-	render() {
-		return (
-			<div>
-				<h1>Booking Button</h1>
-				<input value="Book" type="button" onClick={this.handleDrop}/>
-				{ this.state.drop ? (
-					<div ref={(ele) => this.menu = ele}>
-						<p>Adults</p><input value="-" type="button"/><input value="+" type="button"/>
-						<p>Children</p><input value="-" type="button"/><input value="+" type="button"/>
-						<p>Infants</p><input value="-" type="button"/><input value="+" type="button"/>
-					</div>) : null }
-			</div>
-		)
-	}
+    return (
+      <div>
+        <h1>Booking Button</h1>
+        <input value="Book" type="button" onClick={this.handleDrop} />
+        { drop ? (
+          <div ref={ele => this.menu = ele}>
+            <p>Adults</p>
+            <input value="-" type="button" />
+            <input value="+" type="button" />
+            <p>Children</p>
+            <input value="-" type="button" />
+            <input value="+" type="button" />
+            <p>Infants</p>
+            <input value="-" type="button" />
+            <input value="+" type="button" />
+          </div>) : null }
+      </div>
+    );
+  }
 }
 
 export default Guest;
