dbdb99ba4c5a7f6e737472d9bdeca0a2492612ff
JayBo7 <jvboinapalli@ucdavis.edu>
2018-11-07 11:39:54 -0800

    change to script and index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,3 @@
 import Booking from './components/app.jsx';
-import React from 'react';
-import { render } from 'react-dom';
 
-render(<Booking />, document.getElementById('bookings'));
-
-// window.Booking = Booking;
+window.Booking = Booking;
