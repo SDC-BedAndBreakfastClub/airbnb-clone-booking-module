3881f93b103a65b7597249dfaf55f00952b4bd00
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-11-14 11:32:32 -0800

    Implemented CRUD routes, changed name of API route for listing info and updated client to match.

--- booking-module/server/index.js
+++ booking-module/server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const location = require('../database/Location.js');
+const booking = require('../database/Booking.js');
 
 
 const app = express();
@@ -14,14 +15,18 @@ app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/api/rooms/:listingId/booking', (req, res) => {
-  console.log('PARAMS', req.params);
-  console.log('BODY', req.body);
-  location.find({ id: Number(req.params.listingId) })
+app.get('/api/rooms/:listingId/listingdetails', (req, res) => {
+  location.find({ id: req.params.listingId })
     .exec((error, results) => {
-      console.log(error);
-      console.log(results);
-      res.send(results);
+      res.status(200).send(results);
+    });
+});
+
+app.get('/api/rooms/:listingId/booking', (req, res) => {
+  booking.find({ id: req.params.listingId })
+    .exec((err, results) => {
+      if (err) { return res.sendStatus(500); }
+      return res.status(200).json(results);
     });
 });
 
@@ -30,4 +35,40 @@ app.get('/rooms/:listingId/booking', (req, res) => {
     root: path.join(__dirname, '..', 'client/dist/'),
   };
   res.sendFile('index.html', options);
+});
+
+app.post('/api/rooms/:listingId/booking', (req, res) => {
+  const { body } = req;
+  console.log(body);
+  booking.create({
+    id: req.params.listingId,
+    check_in: body.check_in,
+    check_out: body.check_out,
+    booking_location: body.booking_location,
+    user: body.user,
+  }, (err, data) => {
+    if (err) { return res.sendStatus(500); }
+    return res.status(201).json(data);
+  });
+});
+
+app.patch('/api/rooms/:listingId/booking', (req, res) => {
+  const { body } = req;
+  booking.findByIdAndUpdate(body.bookingId, {
+    check_in: body.check_in,
+    check_out: body.check_out,
+    booking_location: body.booking_location,
+    user: body.user,
+  }, (err, data) => {
+    if (err) { return res.sendStatus(500); }
+    return res.status(209).json(data);
+  });
+});
+
+app.delete('/api/rooms/:listingId/booking', (req, res) => {
+  const { body } = req;
+  booking.findByIdAndDelete(body.bookingId, (err) => {
+    if (err) { return res.sendStatus(500); }
+    return res.sendStatus(204);
+  });
 });
