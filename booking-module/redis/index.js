a6d3b30e491adb5782d1043f3eac596f1a1a4dc6
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-11-29 15:09:48 -0800

    typo fix

--- booking-module/redis/index.js
+++ booking-module/redis/index.js
@@ -1,5 +1,5 @@
-const redis = require(redis);
+const redis = require('redis');
 
-const client = redis.createClient({host: '172.31.35.149'});
+const client = redis.createClient({ host: '172.31.35.149' });
 
 module.exports = client;
